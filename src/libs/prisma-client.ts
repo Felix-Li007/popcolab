1b95391d1cc2da56280ee1afbd97dbd02728c85f
Felix Li <fli5@academic.rrc.ca>
2026-02-13 11:05:06 -0600

    ✨ Refactor Prisma client initialization and remove demo component
    - Updated connection string handling for Prisma client
    - Improved global Prisma client management
    - Removed unused demo component
    
    Signed-off-by: Felix Li <fli5@academic.rrc.ca>

--- src/libs/prisma-client.ts
+++ src/libs/prisma-client.ts
@@ -2,13 +2,18 @@ import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@/libs/prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
-//const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const adapter = new PrismaPg({ connectionString });
-// const prisma = new PrismaClient({ adapter });
 
-const prisma = global.prisma || new PrismaClient({ adapter });
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export { prisma };
