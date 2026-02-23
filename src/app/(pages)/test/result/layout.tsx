3c3e42a30fded56e8e778e5c409699913a76117b
Marion Queen Ramos <mramos3@academic.rrc.ca>
2026-02-23 15:13:57 -0600

    feat: added play personality page and layouts to use new component.

---
+++ src/app/(pages)/test/result/layout.tsx
@@ -0,0 +1,11 @@
+export default function ResultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="max-w-4xl mx-auto px-4 py-12">{children}</div>
+    </div>
+  );
+}
