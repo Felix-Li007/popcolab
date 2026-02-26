f15ee35359103103f95ecc2d9d3eff5a63c339fc
Felix Li <fli5@academic.rrc.ca>
2026-02-26 08:17:28 -0600

    Fixed  a wierd filename issue (#18)
    Fixed a wierd filename issue
    
    Signed-off-by: Felix Li <fil5@academic.rrc.ca>
    Signed-off-by: Felix Li <fli5@academic.rrc.ca>
    Co-authored-by: Felix Li <fil5@academic.rrc.ca>

--- src/app/(pages)/(front)/page.tsx
+++ src/app/(pages)/(front)/page.tsx
@@ -1,14 +1,14 @@
-import HeroSection from '@/components/front/landing-hero';
 import EventsSection from '@/components/front/landing-events';
 import CTASection from '@/components/front/landing-CTASection';
 import ExperiencesCarousel from '@/components/front/landing-experiences';
 import ImageGallerySection from '@/components/front/landing-imagegallery';
+import PersonalitiesSection from '@/components/front/landing-personality';
 
 export default function Home() {
   return (
     <>
       {/* <Header /> */}
-      <HeroSection />
+      <PersonalitiesSection />
       <EventsSection />
       <ExperiencesCarousel />
       <ImageGallerySection />
