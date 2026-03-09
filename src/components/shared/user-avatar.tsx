31bd0407de60ef2ef8dee2b60ddc92a0a53b12e4
Felix Li <253575972+Felix-Li078@users.noreply.github.com>
2026-03-09 14:46:21 -0500

    ✨ Update UserAvatar component to navigate to dynamic dashboard URL (#57)
    Signed-off-by: Felix Li <253575972+Felix-Li078@users.noreply.github.com>

--- src/components/shared/user-avatar.tsx
+++ src/components/shared/user-avatar.tsx
@@ -63,7 +63,11 @@ export default function UserAvatar({
                       />
                     </svg>
                   }
-                  onClick={() => router.push('/dashboard')}
+                  onClick={() =>
+                    router.push(
+                      `${process.env.NEXT_PUBLIC_CLERK_ACTION_DASHBOARD_URL}`
+                    )
+                  }
                 />
               </UserButton.MenuItems>
             </UserButton>
